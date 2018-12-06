b1743c02e6534baa3dcc25072f0c59fe1312dc6d
Márk Marosi <marosim99@gmail.com>
2018-12-06 11:18:22 +0100

    kiírás

--- egészSzámok/egészSzámok/Program.cs
+++ egészSzámok/egészSzámok/Program.cs
@@ -11,7 +11,10 @@ namespace egészSzámok
         static void Main(string[] args)
         {
             int a = 0;
+            Console.WriteLine("a="+a);
             int b = 1;
+            Console.WriteLine("b=" + b);
+            Console.ReadKey();
         }
     }
 }
